921e6cdde0439c96aec4c7b7dee9e90ac4f87a4b
Ray <Ray@192.168.0.100>
2013-05-09 16:33:25 +0800

    将星期参数从路径参数变成查询参数

--- src/main/java/cn/edu/seu/herald/ws/resource/ClassroomResource.java
+++ src/main/java/cn/edu/seu/herald/ws/resource/ClassroomResource.java
@@ -29,7 +29,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -49,10 +48,10 @@ public class ClassroomResource {
     private CsvBuilder csvBuilder = new CsvBuilder();
 
     @GET
-    @Path("/unused/{day}")
+    @Path("/unused")
     @Produces("text/csv")
     public String getUnusedClassrooms(
-            @PathParam("day") Day day,
+            @QueryParam("day") Day day,
             @QueryParam("from") int from,
             @QueryParam("to") int to,
             @Context HttpServletResponse response) {
