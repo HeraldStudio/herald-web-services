4f7db4db1bdaf149971c2fc004cd740961d14bdd
Wenhao Ji <predator.ray@gmail.com>
2013-07-08 21:59:24 +0800

    移除httpclient的cookie存储功能，保障连接独立

--- src/main/java/cn/edu/seu/herald/ws/dao/AbstractHttpDataAccess.java
+++ src/main/java/cn/edu/seu/herald/ws/dao/AbstractHttpDataAccess.java
@@ -1,10 +1,12 @@
 package cn.edu.seu.herald.ws.dao;
 
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.cookie.CookiePolicy;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 
-import javax.xml.ws.http.HTTPException;
 import java.io.IOException;
 
 /**
@@ -16,32 +18,45 @@ abstract class AbstractHttpDataAccess {
 
     public AbstractHttpDataAccess(HttpClient httpClient) {
         this.httpClient = httpClient;
+        httpClient.getParams().setCookiePolicy(CookiePolicy.IGNORE_COOKIES);
     }
 
-    protected String getResponseBody(HttpMethodBase httpMethodBase)
+    protected String getResponseBody(HttpMethod httpMethod)
             throws DataAccessException {
-        int status = executeMethod(httpMethodBase);
+        int status = executeMethod(httpMethod);
         if (status != HttpStatus.SC_OK) {
             throw new DataAccessException("Unexpected status: " + status);
         }
 
         try {
-            return httpMethodBase.getResponseBodyAsString();
+            return httpMethod.getResponseBodyAsString();
         } catch (IOException ex) {
             throw new DataAccessException(ex);
         }
     }
 
-    protected int executeMethod(HttpMethodBase httpMethodBase)
+    protected int executeMethod(HttpMethod httpMethod)
             throws DataAccessException {
         try {
-            return httpClient.executeMethod(httpMethodBase);
+            return httpClient.executeMethod(httpMethod);
         } catch (IOException ex) {
             throw new DataAccessException(ex);
         }
     }
 
-    protected void releaseConnection(HttpMethodBase methodBase) {
-        methodBase.releaseConnection();
+    protected void releaseConnection(HttpMethod httpMethod) {
+        httpMethod.releaseConnection();
+    }
+
+    protected GetMethod newGetMethod(String url) {
+        GetMethod getMethod = new GetMethod(url);
+        getMethod.getParams().setCookiePolicy(CookiePolicy.IGNORE_COOKIES);
+        return getMethod;
+    }
+
+    protected PostMethod newPostMethod(String url) {
+        PostMethod postMethod = new PostMethod(url);
+        postMethod.getParams().setCookiePolicy(CookiePolicy.IGNORE_COOKIES);
+        return postMethod;
     }
 }
