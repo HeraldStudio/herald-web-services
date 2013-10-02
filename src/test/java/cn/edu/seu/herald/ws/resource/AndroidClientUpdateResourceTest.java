acfbabb59e57f534e9c5596d1bc3e0b642d86198
Wenhao Ji <predator.ray@gmail.com>
2013-10-02 15:52:26 +0800

    删除错误的单测

--- src/test/java/cn/edu/seu/herald/ws/resource/AndroidClientUpdateResourceTest.java
+++ src/test/java/cn/edu/seu/herald/ws/resource/AndroidClientUpdateResourceTest.java
@@ -1,31 +1,22 @@
 package cn.edu.seu.herald.ws.resource;
 
-import cn.edu.seu.herald.ws.api.update.Update;
-import cn.edu.seu.herald.ws.dao.AndroidClientUpdateDataAccess;
-import mockit.Expectations;
-import mockit.Mocked;
-import mockit.Tested;
-import mockit.integration.junit4.JMockit;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 /**
  * Copyright (c) 2013 Ray <predator.ray@gmail.com>
  */
-@RunWith(JMockit.class)
 public class AndroidClientUpdateResourceTest {
 
-    @Tested
     private AndroidClientUpdateResource androidClientUpdateResource;
-    @Mocked
-    private AndroidClientUpdateDataAccess androidClientUpdateDataAccess;
+
+    @Before
+    public void setUp() {
+
+    }
 
     @Test
     public void testGetUpdateInfo() throws Exception {
-        new Expectations() {{
-            androidClientUpdateDataAccess.getUpdateInfo();
-            result = new Update();
-        }};
-        androidClientUpdateResource.getUpdateInfo();
+
     }
 }
